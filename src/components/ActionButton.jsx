b53b3540316f693e527209af2ddf5b2a4f2c501f
speshov <stojan.peshov@gmail.com>
2021-11-11 16:35:58 +0100

    simple change

--- src/components/ActionButton.jsx
+++ src/components/ActionButton.jsx
@@ -1,45 +1,54 @@
-import React from 'react';
-import Link from 'gatsby-link';
-import PropTypes from 'prop-types';
+import React from "react";
+import Link from "gatsby-link";
+import PropTypes from "prop-types";
 
-import  { IoArrowGraphUpRight, IoAndroidClose, IoInformation }  from  'react-icons/lib/io/';
+import {
+  IoArrowGraphUpRight,
+  IoAndroidClose,
+  IoInformation,
+} from "react-icons/lib/io/";
 
-
-const ActionButton = ({ 
-  topPx, 
-  rightPx, 
-  classes, 
-  isActive, 
-  isFixed, 
-  onClick, 
+const ActionButton = ({
+  topPx,
+  rightPx,
+  classes,
+  isActive,
+  isFixed,
+  onClick,
   buttonRef,
-  locationPathName }) => 
-{  
+  locationPathName,
+}) => {
   return (
-    <button 
+    <button
       onClick={onClick}
       className={`
         ${classes.block} 
-        ${isActive ? classes.isActive : ''} 
-        ${isFixed ? classes.isFixed : ''}
+        ${isActive ? classes.isActive : ""} 
+        ${isFixed ? classes.isFixed : ""}
       `}
       style={{ top: topPx, right: rightPx }}
       ref={buttonRef}
-      aria-label="Remark"
-    > 
-      {isActive 
-        ? (locationPathName === '/' ? <IoArrowGraphUpRight /> : <IoInformation />) 
-        : <IoAndroidClose />}
+      aria-label="Remark Me"
+    >
+      {isActive ? (
+        locationPathName === "/" ? (
+          <IoArrowGraphUpRight />
+        ) : (
+          <IoInformation />
+        )
+      ) : (
+        <IoAndroidClose />
+      )}
     </button>
-  )
-} 
+  );
+};
 
 ActionButton.propTypes = {
   topPx: PropTypes.string.isRequired,
   rightPx: PropTypes.string.isRequired,
   classes: PropTypes.object.isRequired,
   isActive: PropTypes.bool.isRequired,
-  isFixed: PropTypes.bool.isRequired
-}  
+  isFixed: PropTypes.bool.isRequired,
+};
 
 export default ActionButton;
