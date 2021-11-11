811610267b6a9810462d36831bf66b6fc3b9fbed
speshov <stojan.peshov@gmail.com>
2021-11-11 22:57:46 +0100

    test

--- src/components/ToolBoxList.jsx
+++ src/components/ToolBoxList.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ToolBoxListItem from './ToolBoxListItem';
+import React from "react";
+import ToolBoxListItem from "./ToolBoxListItem";
 
 class ToolBoxList extends React.Component {
   constructor(props) {
     super(props);
     this.onClickHandler = this.onClickHandler.bind(this);
-  }  
-  
+  }
+
   onClickHandler(e, id) {
     this.props.itemOnClick(e, id);
   }
@@ -14,23 +14,19 @@ class ToolBoxList extends React.Component {
   render() {
     return (
       <ul className="c-toolbox-list">
-      {
-        this.props.items.map((item) => (
-          <ToolBoxListItem 
+        {this.props.items.map((item) => (
+          <ToolBoxListItem
             id={item.id}
             key={item.id}
-            label={item.label}  
-            onClick={this.onClickHandler} 
+            label={item.label}
+            onClick={this.onClickHandler}
             activated={this.props.activatedItem}
-            popOverPosition={this.props.popOverPosition}   
+            popOverPosition={this.props.popOverPosition}
           />
-        ))
-      }
+        ))}
       </ul>
-    )
+    );
   }
-};
-
+}
 
 export default ToolBoxList;
-
