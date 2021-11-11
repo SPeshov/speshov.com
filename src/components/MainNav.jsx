623e3543189264c11f9b00a623258bb354186520
speshov <stojan.peshov@gmail.com>
2021-11-11 23:01:56 +0100

    test label

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,22 +1,27 @@
-import React from 'react';
-import Link from 'gatsby-link';
-import { FaChevronDown, FaChevronUp } from 'react-icons/lib/fa';
-import  { IoIosHomeOutline, IoPaperAirplane, IoWrench, IoEmail }  from  'react-icons/lib/io/';
-import { TiCodeOutline } from 'react-icons/lib/ti';
+import React from "react";
+import Link from "gatsby-link";
+import { FaChevronDown, FaChevronUp } from "react-icons/lib/fa";
+import {
+  IoIosHomeOutline,
+  IoPaperAirplane,
+  IoWrench,
+  IoEmail,
+} from "react-icons/lib/io/";
+import { TiCodeOutline } from "react-icons/lib/ti";
 
 const ListLink = (props) => (
-  <li className="c-main-nav__elem" >
-    <Link 
-      to={props.to} 
-      className="c-main-nav__link" 
-      activeClassName="c-main-nav__link--is-active" 
+  <li className="c-main-nav__elem">
+    <Link
+      to={props.to}
+      className="c-main-nav__link"
+      activeClassName="c-main-nav__link--is-active"
       exact={true}
       onClick={props.closeMenu}
     >
       {props.children}
     </Link>
   </li>
-);  
+);
 
 class MainNav extends React.Component {
   constructor(props) {
@@ -25,78 +30,78 @@ class MainNav extends React.Component {
     this.closeMenu = this.closeMenu.bind(this);
     this.state = {
       links: [
-        { to: '/', text: 'Home', icon: IoIosHomeOutline },
-        { to: '/toolbox/', text: 'Toolbox', icon: IoWrench },
-        { to: '/playground/', text: 'Playground', icon: IoPaperAirplane },
-        { to: '/contact/', text: 'Contact', icon: IoEmail }
+        { to: "/", text: "Home", icon: IoIosHomeOutline },
+        { to: "/toolbox/", text: "Toolbox", icon: IoWrench },
+        { to: "/playground/", text: "Playground", icon: IoPaperAirplane },
+        { to: "/contact/", text: "Contact", icon: IoEmail },
       ],
-      mainNavModifierClassName: '',
-      mobileDetailsNav: null
-    }
+      mainNavModifierClassName: "",
+      mobileDetailsNav: null,
+    };
   }
 
   closeMenu() {
-    console.log('closeMenu()');
+    console.log("closeMenu()");
     if (this.state.mobileDetailsNav) {
       this.setState(() => ({
-        mainNavModifierClassName: '',
-        mobileDetailsNav: null
-      }))
+        mainNavModifierClassName: "",
+        mobileDetailsNav: null,
+      }));
     }
   }
 
   toggleMenu() {
     let mobileDetailsNav = null;
-    let mainNavModifierClassName = '';
+    let mainNavModifierClassName = "";
 
     if (!this.state.mobileDetailsNav) {
       mobileDetailsNav = (
         <div className="c-main-nav c-main-nav--is-open">
-        { 
-          this.state.links.map((link, i) => (
-            <ListLink closeMenu={this.closeMenu} to={link.to} key={i} >
-              <link.icon  />
+          {this.state.links.map((link, i) => (
+            <ListLink closeMenu={this.closeMenu} to={link.to} key={i}>
+              <link.icon />
               <span className="c-main-nav__text">{link.text}</span>
-            </ListLink>  
-          )) 
-        }
+            </ListLink>
+          ))}
         </div>
       );
-      mainNavModifierClassName = 'c-main-nav--is-hidden'
-    } 
+      mainNavModifierClassName = "c-main-nav--is-hidden";
+    }
 
     this.setState(() => ({
       mainNavModifierClassName: mainNavModifierClassName,
-      mobileDetailsNav: mobileDetailsNav
+      mobileDetailsNav: mobileDetailsNav,
     }));
   }
-  
+
   render() {
     return (
       <div>
-        <ul className={ `c-main-nav ${this.state.mainNavModifierClassName}` }>
-          { 
-            this.state.links.map((link, i) => (
-              <ListLink to={link.to} key={i} testLink={this.testLink}>
-                <link.icon />
-                <span className="c-main-nav__text">{link.text}</span>
-              </ListLink>  
-            )) 
-          }
-          <li 
-            className="c-main-nav__elem c-main-nav__elem--close-link" 
+        <ul className={`c-main-nav ${this.state.mainNavModifierClassName}`}>
+          {this.state.links.map((link, i) => (
+            <ListLink to={link.to} key={i} testLink={this.testLink}>
+              <link.icon />
+              <span className="c-main-nav__text">{link.text}</span>
+            </ListLink>
+          ))}
+          <li
+            className="c-main-nav__elem c-main-nav__elem--close-link"
             onClick={this.toggleMenu}
           >
             <a href="#" className="c-main-nav__link">
-              { (this.state.mainNavModifierClassName) ? <FaChevronUp /> : <FaChevronDown />  }
+              {this.state.mainNavModifierClassName ? (
+                <FaChevronUp />
+              ) : (
+                <FaChevronDown />
+              )}
               <span className="c-main-nav__text">Close</span>
             </a>
-          </li>    
-        </ul> 
-        { this.state.mobileDetailsNav }
+          </li>
+        </ul>
+        {this.state.mobileDetailsNav}
       </div>
-    )
+    );
   }
-};
+}
 
 export default MainNav;
--- src/containers/RemarkScreenContainer.jsx
+++ src/containers/RemarkScreenContainer.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
-import { toggleRemarkScreen } from '../state/actions'; 
-import RemarkScreen from '../components/RemarkScreen';
+import { toggleRemarkScreen } from "../state/actions";
+import RemarkScreen from "../components/RemarkScreen";
 
 class RemarkScreenContainer extends React.Component {
-  constructor(props) {  
+  constructor(props) {
     super(props);
   }
 
@@ -14,15 +14,15 @@ class RemarkScreenContainer extends React.Component {
   }
 
   initializeEscClosing() {
-    if (typeof window !== 'undefined') {
-      window.addEventListener('keydown', (e) => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("keydown", (e) => {
         if (this.props.isActive && e.which == 27) {
           this.props.toggleScreen();
-        } 
+        }
       });
-    } 
+    }
   }
-  
+
   render() {
     return (
       <div>
@@ -31,22 +31,26 @@ class RemarkScreenContainer extends React.Component {
           locationPathName={this.props.locationPathName}
           onClick={this.props.toggleScreen}
         />
-        {this.props.isActive && <div 
+        {this.props.isActive && (
+          <div
             onClick={this.props.toggleScreen}
             className="c-remark-screen-overlay"
-          >
-          </div>}
+          ></div>
+        )}
       </div>
-    )
+    );
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
   return { isActive: state.remarkScreen.isActive };
-}
+};
 
 const mapDispatchToProps = (dispatch) => {
   return { toggleScreen: () => dispatch(toggleRemarkScreen()) };
-}
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(RemarkScreenContainer);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(RemarkScreenContainer);
